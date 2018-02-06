b196482a78a6081be305fba829713ec5416ef400
vmartinovich <mv26091989@gmail.com>
2018-02-06 18:45:14 +0300

    Initial commit

---
+++ src/main/java/org/springboot/resttemplate/tss/Starter.java
@@ -0,0 +1,14 @@
+package org.springboot.resttemplate.tss;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class Starter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Starter.class, args);
+
+    }
+
+}
